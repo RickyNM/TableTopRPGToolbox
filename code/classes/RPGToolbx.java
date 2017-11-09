7e65258f63838c56aea3a9a513e1d38006a56efe
RickyNM <33374250+RickyNM@users.noreply.github.com>
2017-11-09 13:38:55 -0500

    Update RPGToolbx.java

--- code/classes/RPGToolbx.java
+++ code/classes/RPGToolbx.java
@@ -1,1 +1,77 @@
+public class RPGToolbox
+{
+  private String name;
+  private String description;
+  private String picture;
+
+  public TTRPGToolbox()
+  {
+    name = "";
+    description = "";
+    picture = "";
+  }
+
+  public TTRPGToolbox(String newName, String newDescription, String newPicture)
+  {
+    name = newName;
+    description = newDescription;
+    picture = newPicture;
+  }
+
+  public void setName(String newName)
+  {
+    if (newName != null && !newName.trim().equals("") )
+    {
+      name = newName;
+    }
+    else
+    {
+      System.out.println("There is no name.");
+    }
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setDescription(String newDescription)
+  {
+    if (newDescription != null)
+    {
+      description = newDescription;
+    }
+    else
+    {
+      System.out.println("There is no description.");
+    }
+  }
+
+  public String getDescription()
+  {
+    return description;
+  }
+
+  public void setPicture(String newPicture)
+  {
+    if (newPicture != null && !newPicture.trim().equals("") )
+    {
+      picture = newPicture;
+    }
+    else
+    {
+      System.out.println("There is no picture.");
+    }
+  }
+
+  public String getPicture()
+  {
+    return picture;
+  }
+
+  public void writeTo(String fileName)
+  {
+    //stuff
+  }
+}
 
