67c4a8d8f0045c299b6761cc07e25862fdf1af8a
RickyNM <33374250+RickyNM@users.noreply.github.com>
2017-11-15 00:00:33 -0500

    Update Armor.java

--- code/classes/Armor.java
+++ code/classes/Armor.java
@@ -1,1 +1,52 @@
 
+public class Armor extends Item
+{
+  private String armorClass;
+  private boolean stealth;
+
+  public Armor()
+  {
+    super();
+    armorClass = "10 + Dex";
+    stealth = false;
+  }
+
+  public Armor(String newName, String newDescription, String newPicture,  String newCost, int newWeight, String newArmorClass, boolean newStealth)
+  {
+    super(newName, newDescription, newPicture, newCost, newWeight);
+    armorClass = newArmorClass;
+    stealth = newStealth;
+  }
+
+  public String getArmorClass()
+  {
+    return armorClass;
+  }
+
+  public void setArmorClass(String newArmorClass)
+  {
+    if (newArmorClass != null)
+    {
+      armorClass = newArmorClass;
+    }
+    else
+    {
+      System.out.println("There is no armorClass.");
+    }
+  }
+
+  public boolean getStealth()
+  {
+    return stealth;
+  }
+
+  public void setStealth(boolean newStealth)
+  {
+      stealth = newStealth;
+  }
+
+  public void writeTo(String fileabilities)
+  {
+    //stuff
+  }
+}
