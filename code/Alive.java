cb7262a4aedef0b0b160bd4248dd4c5e315b548d
Strider Jonak <33399977+StriderJonak@users.noreply.github.com>
2017-11-18 22:08:36 -0500

    Update Alive.java

--- code/Alive.java
+++ code/Alive.java
@@ -1,81 +1,93 @@
+/*
+ *  Aliva.java
+ *
+ *  Author: Strider Jonak
+ *  Version: 11.18.2017
+ *
+ *  Superclass for living creatures that will extend
+ *  both Character and Monster classes.
+ */
+
 public class Alive {
 
-  public String name;
-  public int healthMax;
-  public int healthNow;
-  public int experience;
-  public int armorClass;
-  public int moveSpeed;
+  public String Name;
+  public int    MaxHealth;
+  public int    CurrentHealth;
+  public int    ArmorClass;
+  public int    MoveSpeed;
+  public int    Experience;
 
-  public inventory[] = new Item[];
-  public spellList[] = new SpellAbility[];
+  public ItemList[]   = new Item[];
+  public SpellList[]  = new SpellAbility[];
 
   // CONSTRUCTOR
-  public Alive(String name, int HP, int AC, int MS) {
-    healthMax   = HP;
-    healthNow   = HP;
-    experience  = 0;
-    armorClass  = AC;
-    moveSpeed   = MS;
+  public Alive(String N, int HP, int AC, int MS) {
+    Name          = N;
+    MaxHealth     = H;
+    CurrentHealth = H;
+    ArmorClass    = AC;
+    MoveSpeed     = MS;
+    Experience    = 0;
   }
 
   /*
    *  getName - return String of name
    */
    public String getName() {
-     return name;
+     return Name;
    }
 
-   public void setName(String rename) {
-     name = rename;
+   public void setName(String _Name) {
+     Name = _Name;
    }
 
-   public int getHealthMax() {
-     return healthMax;
+   public int getMaxHealth() {
+     return MaxHealth;
    }
 
-   public void setHealthMax(int health) {
-     healthMax = health;
+   public void setHealthMax(int _MaxHealth) {
+     MaxHealth = _MaxHealth;
    }
 
-   public int getHealthNow() {
-     return healthNow;
+   public int getCurrentHealth() {
+     return CurrentHealth;
    }
 
-   public void setHealthNow(int health) {
-     healthNow = health;
+   public void setCurrentHealth(int _CurrentHeath) {
+     CurrentHealth = _CurrentHealth;
    }
 
    public int getExperience() {
-     return healthNow;
+     return Experience;
    }
 
-   public void setExperience(int exp) {
-     experience = exp;
+   public void setExperience(int _Experience) {
+     Experience = _Experience;
    }
 
    public int getArmorClass() {
-     return armorClass;
+     return ArmorClass;
    }
 
-   public void setArmorClass(int AC) {
-     armorClass = AC;
+   public void setArmorClass(int _ArmorClass) {
+     ArmorClass = _ArmorClass;
    }
 
    public int getMoveSpeed() {
-     return moveSpeed;
+     return MoveSpeed;
    }
 
-   public void setMoveSpeed(int MS) {
-     moveSpeed = MS;
+   public void setMoveSpeed(int _MoveSpeed) {
+     MoveSpeed = _MoveSpeed;
    }
 
   /*
    *  toString()  - return a String representation of the Alive object
    */
   public String toString() {
-    return name + "\tHealth: " + healthNow + "/" + healthMax + "\n"
-                + armorClass + "AC\t" + moveSpeed + " Speed\n"
-                + experience + "EXP";
+    return  Name +"\n"+
+            CurrentHealth +"/"+     MaxHealth +"HP\n"+
+            ArmorClass    +"AC\t"+  MoveSpeed +" Speed\n"+
+            Experience +"EXP";
   }
 }
