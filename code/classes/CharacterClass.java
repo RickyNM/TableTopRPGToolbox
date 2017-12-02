3669e4e89a548e77458dc0028c4e98ba16cf6b2b
RickyNM <33374250+RickyNM@users.noreply.github.com>
2017-12-02 12:18:09 -0500

    Update CharacterClass.java

--- code/classes/CharacterClass.java
+++ code/classes/CharacterClass.java
@@ -1,11 +1,10 @@
-
 import java.util.ArrayList;
 
 public class CharacterClass extends RPGToolbox
 {
   private ArrayList<SpellAbility> abilities;
   private ArrayList<Item> items;
-  private String health;
+  private int hitDie;
   private ArrayList<String> proficiencies;
 
   public CharacterClass()
@@ -13,16 +12,16 @@ public class CharacterClass extends RPGToolbox
     super();
     abilities = new ArrayList<SpellAbility>();
     items = new ArrayList<Item>();
-    health = "";
+    hitDie = 6;
     proficiencies = new ArrayList<String>();
   }
 
-  public CharacterClass(String newName, String newDescription, String newPicture, ArrayList<SpellAbility> newAbilities, ArrayList<Item> newItems, String newHealth, ArrayList<String> newProficiencies)
+  public CharacterClass(String newName, String newDescription, String newPicture, ArrayList<SpellAbility> newAbilities, ArrayList<Item> newItems, int newHitDie, ArrayList<String> newProficiencies)
   {
     super(newName, newDescription, newPicture);
     abilities = newAbilities;
     items = newItems;
-    health = newHealth;
+    hitDie = newHitDie;
     proficiencies = newProficiencies;
   }
 
@@ -43,7 +42,7 @@ public class CharacterClass extends RPGToolbox
     return abilities;
   }
 
-  public void setItems(ArrayList<Item> newItems)
+  public void setAbilitys(ArrayList<Item> newItems)
   {
     if (newItems != null)
     {
@@ -60,21 +59,21 @@ public class CharacterClass extends RPGToolbox
     return items;
   }
 
-  public void setHealth(String newHealth)
+  public void setHitDie(int newHitDie)
   {
-    if (newHealth != null && !newHealth.trim().equals("") )
+    if (newHitDie > 0)
     {
-      health = newHealth;
+      hitDie = newHitDie;
     }
     else
     {
-      System.out.println("There is no health.");
+      System.out.println("There is no hitDie.");
     }
   }
 
-  public String getHealth()
+  public int getHitDie()
   {
-    return health;
+    return hitDie;
   }
 
   public void setProficiencies(ArrayList<String> newProficiencies)
@@ -94,8 +93,166 @@ public class CharacterClass extends RPGToolbox
     return proficiencies;
   }
 
-  public void writeTo(String fileabilities)
+  public void addAbility(SpellAbility newAbility)
   {
-    //stuff
+    if (newAbility != null)
+    {
+      abilities.add(newAbility);
+    }
+  }
+
+  public void setAbility(SpellAbility newAbility, int i)
+  {
+    if (i < 0 || i > abilities.size() || newAbility == null)
+    {
+      System.out.println("Invalid set conditions");
+    }
+    else
+    {
+      abilities.set(i, newAbility);
+    }
+  }
+
+  public SpellAbility getAbility(int i)
+  {
+    if (i < 0 || i > abilities.size())
+    {
+      System.out.println("That is outside the list");
+      return null;
+    }
+    return abilities.get(i);
+  }
+
+  public SpellAbility removeAbility(SpellAbility oldAbility)
+  {
+    if (oldAbility != null)
+    {
+      for (int i = 0; i < abilities.size(); i++)
+      {
+        if (oldAbility.getName().equals(abilities.get(i).getName()))
+        {
+          return abilities.remove(i);
+        }
+      }
+    }
+    return null;
+  }
+
+  public SpellAbility removeAbility(int i)
+  {
+    if (i >= 0 && i < abilities.size())
+    {
+      return abilities.remove(i);
+    }
+    return null;
+  }
+
+  public void addItem(Item newItem)
+  {
+    if (newItem != null)
+    {
+      items.add(newItem);
+    }
+  }
+
+  public void setItem(Item newItem, int i)
+  {
+    if (i < 0 || i > items.size() || newItem == null)
+    {
+      System.out.println("Invalid set conditions");
+    }
+    else
+    {
+      items.set(i, newItem);
+    }
+  }
+
+  public Item getItem(int i)
+  {
+    if (i < 0 || i > items.size())
+    {
+      System.out.println("That is outside the list");
+      return null;
+    }
+    return items.get(i);
+  }
+
+  public Item removeItem(Item oldItem)
+  {
+    if (oldItem != null)
+    {
+      for (int i = 0; i < items.size(); i++)
+      {
+        if (oldItem.getName().equals(items.get(i).getName()))
+        {
+          return items.remove(i);
+        }
+      }
+    }
+    return null;
+  }
+
+  public Item removeItem(int i)
+  {
+    if (i >= 0 && i < items.size())
+    {
+      return items.remove(i);
+    }
+    return null;
+  }
+
+  public void addProficiency(String newProficiency)
+  {
+    if (newProficiency != null)
+    {
+      proficiencies.add(newProficiency);
+    }
+  }
+
+  public void setProficiency(String newProficiency, int i)
+  {
+    if (i < 0 || i > proficiencies.size() || newProficiency == null || newProficiency.trim().equals(""))
+    {
+      System.out.println("Invalid set conditions");
+    }
+    else
+    {
+      proficiencies.set(i, newProficiency);
+    }
+  }
+
+  public String removeProficiciency(String oldProficiency)
+  {
+    if (oldProficiency != null)
+    {
+      for (int i = 0; i < proficiencies.size(); i++)
+      {
+        if (oldProficiency.equals(proficiencies.get(i)))
+        {
+          return proficiencies.remove(i);
+        }
+      }
+    }
+    return null;
+  }
+
+  public String removeProficiciency(int i)
+  {
+    if (i >= 0 && i < proficiencies.size())
+    {
+      return proficiencies.remove(i);
+    }
+    return null;
+  }
+
+  public String toString()
+  {
+    String outPut = super.toString();
+    outPut = "Type: CharacterClass\n" + outPut;
+    outPut += "Abilities: " + abilities.toString() + "\n";
+    outPut += "Items" + items.toString() + "\n";
+    outPut += "HitDie: " + String.valueOf(hitDie) + "\n";
+    outPut += "Proficiencies: " + proficiencies.toString() + "\n";
+    return outPut;
   }
 }
