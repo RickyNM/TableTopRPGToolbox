2cc53897ecfcc228cd32d1fc224b46b972aaa2a1
RickyNM <33374250+RickyNM@users.noreply.github.com>
2017-12-15 11:08:44 -0500

    Update Background.java

--- code/classes/Background.java
+++ code/classes/Background.java
@@ -1,12 +1,20 @@
+/*
+Background.java
+Author: Ricky Morash
+Version: 12.14.2017
+
+Background is a subclass of RPGToolbox that is ment to handle all the different backgrounds a character can have
+*/
+
 import java.util.ArrayList;
 
 public class Background extends RPGToolbox
 {
-  private SpellAbility ability;
-  private ArrayList<String> characterTraits;
-  private ArrayList<ArrayList<String>> suggestions;
-  private ArrayList<Item> items;
-  private ArrayList<String> proficiencies;
+  private SpellAbility ability; // the ability of the Background
+  private ArrayList<String> characterTraits; // the ArrayList of different character traits that there are suggestions for
+  private ArrayList<ArrayList<String>> suggestions; // the ArrayList of ArrayLists of suggestions for each character trait
+  private ArrayList<Item> items; // the list of Item that the background starts with
+  private ArrayList<String> proficiencies; // the list of proficiencies that the background starts with
 
   public Background()
   {
@@ -28,6 +36,10 @@ public class Background extends RPGToolbox
     proficiencies = newProficiencies;
   }
 
+/* 
+setAbility sets the ability
+It checks it the SpellAbility is null
+*/
   public void setAbility(SpellAbility newAbility)
   {
     if (newAbility != null)
@@ -40,11 +52,19 @@ public class Background extends RPGToolbox
     }
   }
 
+/*
+getAbility returns the ability
+*/
   public SpellAbility getAbility()
   {
     return ability;
   }
 
+/*
+addCharacterTrait adds a new character trait
+It checks if the string is null or blank
+Also adds a new ArrayList to suggestions
+*/
   public void addCharacterTrait(String newCharacterTrait)
   {
     if (newCharacterTrait != null && !newCharacterTrait.trim().equals(""))
@@ -58,7 +78,11 @@ public class Background extends RPGToolbox
     }
   }
 
-  public void setCharacterTraits(String newCharacterTrait, int i)
+/*
+setCharacterTrait takes an index and changes the character trait at that index 
+It checks if the string is null or blank and if the index is valid
+*/
+  public void setCharacterTrait(String newCharacterTrait, int i)
   {
     if (newCharacterTrait != null && !newCharacterTrait.trim().equals("") && i > 0 && i < characterTraits.size())
     {
@@ -70,11 +94,15 @@ public class Background extends RPGToolbox
     }
   }
 
+/*
+setSuggestion takes two indecies and changes the suggestion at that location
+It checks if the indecies are valid and if the string is null or blank
+*/
   public void setSuggestion(String newSuggestion, int i, int j)
   {
     if (newSuggestion != null && !newSuggestion.trim().equals("") && i > 0 && i < suggestions.size() && j > 0 && j < suggestions.get(i).size())
     {
-      suggestions.get(i).set(j, newSuggestion);
+      suggestions.get(i).set(j, newSuggestion.trim());
     }
     else
     {
@@ -82,11 +110,15 @@ public class Background extends RPGToolbox
     }
   }
 
+/*
+addSuggestion adds a suggestion to the given ArrayList
+It checks if the string is null or blank and if the index is valid
+*/
   public void addSuggestion(String newSuggestion, int i)
   {
     if (newSuggestion != null && !newSuggestion.trim().equals("") && i > 0 && i < suggestions.size())
     {
-      suggestions.get(i).add(newSuggestion);
+      suggestions.get(i).add(newSuggestion.trim());
     }
     else
     {
@@ -94,6 +126,10 @@ public class Background extends RPGToolbox
     }
   }
 
+/*
+removeSuggestion takes two indecies and removes the suggestion at that location
+It checks if the indecies are valid
+*/
   public String removeSuggestion(int i, int j)
   {
     if (i > 0 && i < suggestions.size() && j > 0 && j < suggestions.get(i).size())
@@ -103,6 +139,10 @@ public class Background extends RPGToolbox
     return null;
   }
 
+/*
+removeCharacterTrait takes an index and removes the character trait and suggestions for it
+It checks if the index is valid
+*/
   public ArrayList<String> removeCharacterTrait(int i)
   {
     if (i >= 0 && i < characterTraits.size())
@@ -113,6 +153,10 @@ public class Background extends RPGToolbox
     return null;
   }
 
+/*
+setItems sets the ArrayList of Items
+It checks if the ArrayList is null
+*/
   public void setItems(ArrayList<Item> newItems)
   {
     if (newItems != null)
@@ -125,11 +169,21 @@ public class Background extends RPGToolbox
     }
   }
 
+/*
+getItems returns the ArrayList of Items 
+It is commented out so that the user cannot get the ArrayList
+*/
+/*
   public ArrayList<Item> getItems()
   {
     return items;
   }
+*/
 
+/*
+setProficiencies sets the ArrayList of proficiencies
+It checks if the ArrayList is null
+*/
   public void setProficiencies(ArrayList<String> newProficiencies)
   {
     if (newProficiencies != null)
@@ -142,11 +196,21 @@ public class Background extends RPGToolbox
     }
   }
 
+/*
+getProficiencies returns the ArrayList of the proficiencies
+It is commented out so that the user cannot get the ArrayList
+*/
+/*
   public ArrayList<String> getProficiencies()
   {
     return proficiencies;
   }
+*/
 
+/*
+addItem adds a new item
+It checks if the Item is null
+*/
   public void addItem(Item newItem)
   {
     if (newItem != null)
@@ -155,6 +219,10 @@ public class Background extends RPGToolbox
     }
   }
 
+/*
+setItem sets the Item at the given index 
+It checks if the Item is null and if the index is valid
+*/
   public void setItem(Item newItem, int i)
   {
     if (i < 0 || i > items.size() || newItem == null)
@@ -167,6 +235,10 @@ public class Background extends RPGToolbox
     }
   }
 
+/*
+getItem akes an index and returns the Item 
+It checks if the index is valid
+*/
   public Item getItem(int i)
   {
     if (i < 0 || i > items.size())
@@ -177,6 +249,10 @@ public class Background extends RPGToolbox
     return items.get(i);
   }
 
+/*
+removeItem takes an Item and removes it from the ArrayList
+It checks if the Item is null and returns null if it is not in the ArrayList
+*/
   public Item removeItem(Item oldItem)
   {
     if (oldItem != null)
@@ -192,6 +268,10 @@ public class Background extends RPGToolbox
     return null;
   }
 
+/*
+removeItem takes an index and removes the connected Item 
+It checks if the index is valid
+*/
   public Item removeItem(int i)
   {
     if (i >= 0 && i < items.size())
@@ -201,14 +281,22 @@ public class Background extends RPGToolbox
     return null;
   }
 
+/*
+addProficiency adds a proficiency 
+It checks if the string is null or blank
+*/
   public void addProficiency(String newProficiency)
   {
-    if (newProficiency != null)
+    if (newProficiency != null && !newProficiency.trim().equals(""))
     {
-      proficiencies.add(newProficiency);
+      proficiencies.add(newProficiency.trim());
     }
   }
 
+/*
+setProficiency sets the proficiency at the given index 
+It checks if the index is valid and if the string is null or blank
+*/
   public void setProficiency(String newProficiency, int i)
   {
     if (i < 0 || i > proficiencies.size() || newProficiency == null || newProficiency.trim().equals(""))
@@ -217,10 +305,14 @@ public class Background extends RPGToolbox
     }
     else
     {
-      proficiencies.set(i, newProficiency);
+      proficiencies.set(i, newProficiency.trim());
     }
   }
 
+/*
+getProficiency returns the proficiency at the given index 
+It checks if the index is valid
+*/
   public String getProficiency(int i)
   {
     if (i < 0 || i > proficiencies.size())
@@ -231,6 +323,10 @@ public class Background extends RPGToolbox
     return proficiencies.get(i);
   }
 
+/*
+removeProficiciency takes a string and removes the proficiency
+It checks if the string is null and returns null if it is not in the ArrayList
+*/
   public String removeProficiciency(String oldProficiency)
   {
     if (oldProficiency != null)
@@ -246,6 +342,10 @@ public class Background extends RPGToolbox
     return null;
   }
 
+/*
+removeProficiciency takes an index and removes the connected proficiency
+It checks if the index is valid
+*/
   public String removeProficiciency(int i)
   {
     if (i >= 0 && i < proficiencies.size())
@@ -255,6 +355,9 @@ public class Background extends RPGToolbox
     return null;
   }
 
+/*
+toString converts the Background to a string to be writen to a file
+*/
   public String toString()
   {
     String outPut = super.toString();
@@ -267,4 +370,3 @@ public class Background extends RPGToolbox
     return outPut;
   }
 }
-
