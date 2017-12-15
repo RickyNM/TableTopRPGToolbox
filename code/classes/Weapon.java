92ad5b351dc5d244ffa3dba1ad5f8a99abd11eb2
RickyNM <33374250+RickyNM@users.noreply.github.com>
2017-12-14 19:42:02 -0500

    Update Weapon.java

--- code/classes/Weapon.java
+++ code/classes/Weapon.java
@@ -1,10 +1,17 @@
+/*
+Weapon.java
+Author: Ricky Morash
+Version: 12.14.2017
+
+Weapon is a subclass of Item that is ment to deal with all weapons.
+*/
 
 public class Weapon extends Item
 {
-  private String damage;
-  private String damageType;
-  private String range;
-  private String hands;
+  private String damage; // the string representation of the damage in the form of # + (Dex/Str)
+  private String damageType; // the string of the type of damage
+  private String range; // the string representation of the range in the form of #, #/#, or # #/#
+  private String hands; // the string representation of the number of hands used for the weapon
 
   public Weapon()
   {
@@ -24,11 +31,18 @@ public class Weapon extends Item
     hands = newHands;
   }
 
+/*
+getHands returnd the hands variable
+*/
   public String getHands()
   {
     return hands;
   }
 
+/*
+setHands sets the hands variable
+It checks if the string is null or blank
+*/
   public void setHands(String newHands)
   {
     if (newHands != null && !newHands.trim().equals(""))
@@ -42,6 +56,11 @@ public class Weapon extends Item
     }
   }
 
+/*
+setDamage sets the damage
+It checks if the string is null or blank
+Should also check if the string is of the apropriate form
+*/
   public void setDamage(String newDamage)
   {
     if (newDamage != null && !newDamage.trim().equals(""))
@@ -55,11 +74,18 @@ public class Weapon extends Item
     }
   }
 
+/*
+getDamage returns the damage
+*/
   public String getDamage()
   {
     return damage;
   }
 
+/*
+setDamageType sets the damage type
+It checks if the string is null or blank
+*/
   public void setDamageType(String newDamageType)
   {
     if (newDamageType != null && !newDamageType.trim().equals(""))
@@ -73,16 +99,26 @@ public class Weapon extends Item
     }
   }
 
+/*
+getDamageType returns the damage type
+*/
   public String getDamageType()
   {
     return damageType;
   }
 
+/*
+getRange returns the range
+*/
   public String getRange()
   {
     return range;
   }
 
+/*
+setRange sets the range
+It checks if the string is null or blank
+*/
   public void setRange(String newRange)
   {
     if (newRange != null && !newRange.trim().equals(""))
@@ -96,6 +132,9 @@ public class Weapon extends Item
     }
   }
 
+/*
+toString converts the weapon to a string to be writen to a file
+*/
   public String toString()
   {
     String outPut = super.toString();
@@ -107,4 +146,3 @@ public class Weapon extends Item
     return outPut;
   }
 }
-
