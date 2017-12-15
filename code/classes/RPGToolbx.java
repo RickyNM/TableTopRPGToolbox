a960997e7e353a58ac593d750c6d3ab3290d0f9f
RickyNM <33374250+RickyNM@users.noreply.github.com>
2017-12-14 20:18:41 -0500

    Update RPGToolbx.java

--- code/classes/RPGToolbx.java
+++ code/classes/RPGToolbx.java
@@ -1,8 +1,17 @@
+/*
+RPGToolbox.java
+Author: Ricky Morash
+Version: 12.14.2017
+
+RPGToolbox is the superclass for Alive, Background, CharacterClass, Item, Map, Race, and addSpellAbility
+It is ment to aid in the handling of all the classes
+*/
+
 public class RPGToolbox
 {
-  private String name;
-  private String description;
-  private String picture;
+  private String name; // the name of the object
+  private String description; // the description of the object
+  private String picture; // the string of the path to the picture of the object
 
   public RPGToolbox()
   {
@@ -18,10 +27,15 @@ public class RPGToolbox
     picture = newPicture;
   }
 
+/*
+setName sets the name
+It checks if the string is null or blank
+*/
   public void setName(String newName)
   {
     if (newName != null && !newName.trim().equals("") )
     {
+      newName = newName.trim();
       name = newName;
     }
     else
@@ -30,11 +44,18 @@ public class RPGToolbox
     }
   }
 
+/*
+getName returns the name
+*/
   public String getName()
   {
     return name;
   }
 
+/*
+setDescription sets the description
+It checks if the string is null
+*/
   public void setDescription(String newDescription)
   {
     if (newDescription != null)
@@ -47,15 +68,24 @@ public class RPGToolbox
     }
   }
 
+/*
+getDescription returns the description
+*/
   public String getDescription()
   {
     return description;
   }
 
+/*
+setPicture sets the picture
+It checks if the string is null or blank
+Should also check if there is a file at the location
+*/
   public void setPicture(String newPicture)
   {
     if (newPicture != null && !newPicture.trim().equals("") )
     {
+      newPicture = newPicture.trim();
       picture = newPicture;
     }
     else
@@ -64,11 +94,17 @@ public class RPGToolbox
     }
   }
 
+/*
+getPicture returns the path to the picture
+*/
   public String getPicture()
   {
     return picture;
   }
 
+/*
+toString converts the RPGToolbox to a string for the purpose of writing it to a file
+*/
   public String toString()
   {
     String outPut = "";
@@ -78,4 +114,3 @@ public class RPGToolbox
     return outPut;
   }
 }
-
